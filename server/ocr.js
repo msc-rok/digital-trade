1189984de3702f8317374983287ec1ddf79cdac8
Scheid <marcel.scheid@accenture.com>
2017-01-07 19:03:56 +0100

    bugfix

--- server/ocr.js
+++ server/ocr.js
@@ -10,10 +10,12 @@ var OCR = function () {};
 
 
 OCR.prototype.saveResult = function (res, client, options, result) {
-    console.log('Before ocr.saveResult')
-    await(client.query(tools.replaceSchema("INSERT INTO $$SCHEMANAME$$.ocr_result(result, receipt, quality, psm, lang, img) " +
+    console.log('Before ocr.saveResult(%s)',result)
+    console.log('json.stringify(result): ',json.stringify(result))
+    /*  await(client.query(tools.replaceSchema("INSERT INTO $$SCHEMANAME$$.ocr_result(result, receipt, quality, psm, lang, img) " +
                     " VALUES ($1,$2,$3,$4,$5,$6)"), [result, null, 100, options.psm, options.l, null]));
-    console.log('After ocr.saveResult')
+        console.log('After ocr.saveResult()')
+    */
     //await(users.auditLog(client, userId, constants.AuditProcess, 'Updated templates (' + name + ')', null, obj.length));
     //await(client.query("COMMIT"));
     //res.json({result: 'Rows received: ' + obj.length});
