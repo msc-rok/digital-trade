45aafdb40f1dd62c5d0c6ba667eb6464908b4995
Scheid <marcel.scheid@accenture.com>
2017-01-07 19:37:18 +0100

    msc

--- server/ocr.js
+++ server/ocr.js
@@ -16,7 +16,7 @@ OCR.prototype.saveResult = function (res, client, options, result) {
     console.log('JSON.stringify(resultJson): ',JSON.stringify(resultJson))
 
     await(client.query(tools.replaceSchema("INSERT INTO $$SCHEMANAME$$.ocr_result(result, receipt, quality, psm, lang, img) " +
-                    " VALUES ($1,$2,$3,$4,$5,$6)"), [JSON.stringify(resultJson), null, 100, options.psm, options.l, null]));
+                    " VALUES ($1,$2,$3,$4,$5,$6)"), [JSON.stringify(resultJson), null, 1.0, options.psm, options.l, null]));
     console.log('After ocr.saveResult()');
     
     //await(users.auditLog(client, userId, constants.AuditProcess, 'Updated templates (' + name + ')', null, obj.length));
