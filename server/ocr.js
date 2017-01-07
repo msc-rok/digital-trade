85d04568908aaba737e7b968f8e2add8b17acf97
Scheid <marcel.scheid@accenture.com>
2017-01-07 17:52:19 +0100

    msc

--- server/ocr.js
+++ server/ocr.js
@@ -1,5 +1,5 @@
 
-'use strict';
+//'use strict';
 
 //var async = require('asyncawait/async');
 const AWAIT = require('asyncawait/await');
--- server/tools.js
+++ server/tools.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 
 const constants = require('../server/constants');
