b042ab0cf4c0262606d506ae3a989a7340feace6
Scheid <marcel.scheid@accenture.com>
2017-01-09 00:24:44 +0100

    msc

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next){
 
 require('./routes')(app);
 
-require('./cloudinary_index.js')(app);
+//require('./cloudinary_index.js')(app);
 
 var runningPortNumber = process.env.PORT || 3000;
 
