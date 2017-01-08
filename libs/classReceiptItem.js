b4d556754a8df57bc6ccb0d67928d015ae154f48
Scheid <marcel.scheid@accenture.com>
2017-01-08 13:12:49 +0100

    msc

--- libs/classReceiptItem.js
+++ libs/classReceiptItem.js
@@ -13,16 +13,21 @@ function ReceiptItem () {
 };
 
 
-ReceiptItem.prototype.save = function (client, name, price, quantity) {
+ReceiptItem.prototype.save = function (client, receipt, name, price, quantity) {
     // INSERT INTO ocr.receipt_item(receipt, id, product, price, quantity) VALUES (?, ?, ?, ?, ?);
     // INSERT INTO ocr.receipt(id, store, "user", amount, date) VALUES (?, ?, ?, ?, ?);
     // INSERT INTO ocr.product(id, name) VALUES (?, ?);
-    var product = await(client.query(tools.replaceSchema("INSERT INTO ocr.product(name) " +
+    
+    console.log("name: ", name);
+    console.log("price: ", price);
+    console.log("quantity: ", quantity);
+
+    var product = await(client.query(tools.replaceSchema("INSERT INTO $$SCHEMANAME$$.product(name) " +
                         "VALUES ($1) RETURNING id;"), [name]));
     console.log("product.id: ", product.rows[0].id);
     
     var receipt_item = await(client.query(tools.replaceSchema("INSERT INTO $$SCHEMANAME$$.receipt_item(receipt, product, price, quantity) " +
-                        " VALUES ($1,$2,$3,$4) RETURNING id;"), [null, product.rows[0].id, price, quantity]));
+                        " VALUES ($1,$2,$3,$4) RETURNING id;"), [receipt, product.rows[0].id, price, quantity]));
     console.log("receipt_item.id: ", receipt_item.rows[0].id);
 };
 
